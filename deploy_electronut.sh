aea959dc0bb0ba663a01d06777d058ccb6a2786b
electronut <info@electronut.in>
2014-12-08 14:47:17 +0530

    added a deploy script that uses rsync

---
+++ deploy_electronut.sh
@@ -0,0 +1,10 @@
+echo ****Building site***
+
+bundle exec jekyll build
+
+echo ****uploading site***
+
+rsync -crv -e ssh --rsync-path=~/bin/rsync _site/ elec7045@electronut.in:html/
+
+# play sound
+afplay /System/Library/Sounds/Basso.aiff 
