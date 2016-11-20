0c468a76d0471f1cd11f865ae13101d4b477a343
electronut <info@electronut.in>
2016-11-20 12:37:36 +0530

    removed all

--- deploy_electronut.sh
+++
@@ -1,6 +0,0 @@
-echo ****uploading site***
-
-rsync -crv -e ssh --rsync-path=~/bin/rsync _site/ elec7045@electronut.in:html/
-
-# play sound
-afplay /System/Library/Sounds/Basso.aiff 
--- posts/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: post-index
-title: Blog Posts
-excerpt: "A List of Blog Posts"
-image:
-  feature: header.jpg
----
